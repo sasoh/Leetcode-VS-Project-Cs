05d892ed3f4ab426f0164eb9a0e901c22b9109b1
Alexander Popov <lxndrppv@gmail.com>
2025-12-29 15:03:33 +0200

    feat: simpler solution

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,50 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 public static class Solution
 {
-    public static int FindCircleNum(int[][] isConnected)
+    private static void DFS(int[][] isConnected, HashSet<int> visited, int root)
     {
-        var possibleGroupStarters = new HashSet<int>();
+        visited.Add(root);
         for (var i = 0; i < isConnected.Length; i++)
         {
-            possibleGroupStarters.Add(i);
+            if (visited.Contains(i) || isConnected[root][i] == 0) continue;
+            DFS(isConnected, visited, i);
         }
+    }
+
+    public static int FindCircleNum(int[][] isConnected)
+    {
         var visited = new HashSet<int>();
-
         int groups = 0;
-        while (possibleGroupStarters.Count > 0)
+        for (var i = 0; i < isConnected.Length; i++)
         {
-            var groupStart = possibleGroupStarters.First();
-            possibleGroupStarters.Remove(groupStart);
-            visited.Add(groupStart);
+            if (visited.Contains(i)) continue;
             groups++;
-
-            var connections = new HashSet<int>();
-            for (var i = groupStart; i < isConnected.Length; i++)
-            {
-                if (isConnected[groupStart][i] == 1)
-                {
-                    connections.Add(i);
-                }
-            }
-
-            while (connections.Count > 0)
-            {
-                var connection = connections.First();
-                connections.Remove(connection);
-                possibleGroupStarters.Remove(connection);
-                visited.Add(connection);
-
-                for (var i = 0; i < isConnected.Length; i++)
-                {
-                    if (isConnected[connection][i] == 1 && !visited.Contains(i))
-                    {
-                        connections.Add(i);
-                    }
-                }
-            }
+            DFS(isConnected, visited, i);
         }
 
         return groups;
