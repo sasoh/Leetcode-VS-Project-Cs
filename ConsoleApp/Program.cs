4889f5759d49a509228e103c24bcaf0e0d5cd26b
Alexander Popov <lxndrppv@gmail.com>
2025-12-30 11:02:13 +0200

    chore: added test data

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public class Solution
 {
+    public IList<int> EventualSafeNodes(int[][] graph)
+    {
+        return new List<int>();
+    }
 }
 
 namespace ConsoleApp
@@ -10,9 +15,34 @@ namespace ConsoleApp
     {
         private static readonly Solution s = new();
 
-
         private static void T1()
         {
+            var graph = new[] {
+                new [] { 1,2 },
+                new [] { 2,3},
+                new [] { 5 },
+                new [] { 0 },
+                new [] { 5 },
+                new int [] { },
+                new int [] { }
+            };
+            var result = s.EventualSafeNodes(graph);
+            Console.WriteLine($"Result = {string.Join(", ", result)}");
+            Console.WriteLine($"Expected = [2, 4, 5, 6]");
+        }
+
+        private static void T2()
+        {
+            var graph = new[] {
+                new [] { 1,2,3,4 },
+                new [] { 1,2 },
+                new [] { 3,4 },
+                new [] { 0,4 },
+                new int [] { }
+            };
+            var result = s.EventualSafeNodes(graph);
+            Console.WriteLine($"Result = {string.Join(", ", result)}");
+            Console.WriteLine($"Expected = [4]");
         }
 
         private static void RunTests()
@@ -21,6 +51,7 @@ namespace ConsoleApp
             Console.WriteLine("Running tests");
 
             T1();
+            T2();
 
             Console.WriteLine("Finished tests");
         }
