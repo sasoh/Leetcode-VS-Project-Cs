30398aab29755aec2e7967ed8cf4318b81d0d2d1
Alexander Popov <lxndrppv@gmail.com>
2025-12-30 10:40:23 +0200

    feat: upated starting state

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 
+public class Solution
+{
+}
+
 namespace ConsoleApp
 {
     public class Program
     {
+        private static readonly Solution s = new();
+
+
+        private static void T1()
+        {
+        }
+
+        private static void RunTests()
+        {
+
+            Console.WriteLine("Running tests");
+
+            T1();
+
+            Console.WriteLine("Finished tests");
+        }
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("log");
+            RunTests();
         }
     }
 }
