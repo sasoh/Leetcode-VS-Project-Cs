52ca13c28d09a5a8eb6c0919f3d6fd5c5e75f5e0
Alexander Popov <lxndrppv@gmail.com>
2025-12-30 12:19:00 +0200

    feat: solution

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,7 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public class Solution
 {
+    public bool CanVisitAllRooms(IList<IList<int>> rooms)
+    {
+        var keys = new HashSet<int> { 0 };
+        var possible = new HashSet<int>(keys);
+        var visited = new HashSet<int>();
+
+        while (possible.Count > 0)
+        {
+            var first = possible.First();
+            possible.Remove(first);
+            visited.Add(first);
+
+            var newKeys = rooms[first];
+            foreach (var key in newKeys)
+            {
+                keys.Add(key);
+                if (visited.Contains(key)) continue;
+                possible.Add(key);
+            }
+        }
+
+        return keys.Count == rooms.Count;
+    }
 }
 
 namespace ConsoleApp
@@ -10,9 +35,41 @@ namespace ConsoleApp
     {
         private static readonly Solution s = new();
 
-
         private static void T1()
         {
+            var graph = new[] {
+                new [] { 1 },
+                new [] { 2 },
+                new [] { 3 },
+                new int [] { },
+            };
+            var result = s.CanVisitAllRooms(graph);
+            Console.WriteLine($"Result = {result}");
+            Console.WriteLine($"Expected = true");
+        }
+
+        private static void T2()
+        {
+            var graph = new[] {
+                new [] { 1, 3 },
+                new [] { 3, 0, 1 },
+                new [] { 2 },
+                new [] { 0 },
+            };
+            var result = s.CanVisitAllRooms(graph);
+            Console.WriteLine($"Result = {result}");
+            Console.WriteLine($"Expected = false");
+        }
+
+        private static void T3()
+        {
+            var graph = new[] {
+                new [] { 1 },
+                new [] { 1 }
+            };
+            var result = s.CanVisitAllRooms(graph);
+            Console.WriteLine($"Result = {result}");
+            Console.WriteLine($"Expected = true");
         }
 
         private static void RunTests()
@@ -20,7 +77,9 @@ namespace ConsoleApp
 
             Console.WriteLine("Running tests");
 
-            T1();
+            //T1();
+            //T2();
+            T3();
 
             Console.WriteLine("Finished tests");
         }
