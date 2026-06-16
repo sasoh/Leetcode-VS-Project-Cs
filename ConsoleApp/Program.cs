5fc17c6442d65606216b223853a5054bd58b9771
Alexander Popov <lxndrppv@gmail.com>
2026-06-16 13:03:02 +0300

    feat: solution to phone number analysis

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 
-public class Solution
+public static class PhoneNumber
 {
+    public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
+    {
+        var numbers = phoneNumber.Split('-');
+        var isNewYork = numbers[0] == "212";
+        var isFake = numbers[1] == "555";
+        return (isNewYork, isFake, numbers[2]);
+    }
+
+    public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo)
+    {
+        return phoneNumberInfo.IsFake;
+    }
 }
 
 namespace ConsoleApp
 {
     public class Program
     {
-        private static readonly Solution s = new();
-
         private static void T1()
         {
         }
