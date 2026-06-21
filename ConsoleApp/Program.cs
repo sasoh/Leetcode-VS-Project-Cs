0a6c31006801dd9092e18d4b6a415f9928d7da15
Alexander Popov <lxndrppv@gmail.com>
2026-06-21 12:23:00 +0300

    chore: cleanup

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,35 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace ConsoleApp
 {
-    static class Appointment
-    {
-        public static DateTime Schedule(string appointmentDateDescription)
-        {
-            if (DateTime.TryParse(appointmentDateDescription, out var parsed)) {
-                return parsed;
-            }
-            return DateTime.Now;
-        }
-
-        public static bool HasPassed(DateTime appointmentDate) => (DateTime.Now - appointmentDate).TotalSeconds > 0;
-
-        public static bool IsAfternoonAppointment(DateTime appointmentDate) => appointmentDate.Hour >= 12 && appointmentDate.Hour < 18;
-
-        public static string Description(DateTime appointmentDate) => $"You have an appointment on {appointmentDate}.";
-
-        public static DateTime AnniversaryDate() => new DateTime(DateTime.Now.Year, 9, 15);
-    }
-
-
     public class Program
     {
         private static void T1()
         {
-            
         }
 
         private static void RunTests()
