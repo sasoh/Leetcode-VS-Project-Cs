7f1986ab6419004fa571884a5f263ecd18596f6a
Alexander Popov <lxndrppv@gmail.com>
2026-06-22 17:17:47 +0300

    feat: solution to accumulate

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,54 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 namespace ConsoleApp
 {
-    public static class Languages
+    public static class AccumulateExtensions
     {
-        public static List<string> NewList() => new();
-
-        public static List<string> GetExistingLanguages() => new() { "C#", "Clojure", "Elm" };
-
-        public static List<string> AddLanguage(List<string> languages, string language)
+        public static IEnumerable<U> Accumulate<T, U>(this IEnumerable<T> collection, Func<T, U> func)
         {
-            languages.Add(language);
-            return languages;
-        }
-
-        public static int CountLanguages(List<string> languages) => languages.Count;
-
-        public static bool HasLanguage(List<string> languages, string language) => languages.Contains(language);
-
-        public static List<string> ReverseList(List<string> languages)
-        {
-            languages.Reverse();
-            return languages;
-        }
-
-        public static bool IsExciting(List<string> languages) => languages.Count > 0 && (languages[0] == "C#" || (languages.Count > 2 && languages.Count < 4 && languages[1] == "C#"));
-
-        public static List<string> RemoveLanguage(List<string> languages, string language)
-        {
-            languages.Remove(language);
-            return languages;
-        }
-
-        public static bool IsUnique(List<string> languages)
-        {
-            for (var i = 0; i < languages.Count; ++i) {
-                for (var j = i + 1; j < languages.Count; ++j) { 
-                    if (languages[i] == languages[j]) return false;
-                }
+            var i = collection.GetEnumerator();
+            while (i.MoveNext())
+            {
+               yield return func(i.Current);
             }
-            return true;
         }
     }
 
     public class Program
     {
-
         private static void T1()
         {
+
+            var p = new List<int> { 1, 2, 3, 4 };
+            Console.WriteLine($"{string.Join(", ", p.Accumulate(i => i * i))}");
         }
 
         private static void RunTests()
