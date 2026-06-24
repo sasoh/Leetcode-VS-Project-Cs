801ce3d7626157c09adbe4051d3c3745d5b480e5
Alexander Popov <lxndrppv@gmail.com>
2026-06-24 09:46:32 +0300

    feat: solution to space age

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,63 +2,19 @@
 using System.Text;
 namespace ConsoleApp
 {
-    public class SpiralMatrix
+    public class SpaceAge
     {
-        public static int[,] GetMatrix(int size)
-        {
-            var r = new int[size, size];
-
-            var top = 0;
-            var bottom = size - 1;
-            var left = 0;
-            var right = size - 1;
-            var num = 1;
-            while (top <= bottom && left <= right)
-            {
-                for (var i = left; i <= right; ++i)
-                {
-                    r[top, i] = num++;
-                }
-                top++;
-
-                for (var i = top; i <= bottom; ++i)
-                {
-                    r[i, right] = num++;
-                }
-                right--;
-
-                for (var i = right; i >= left; --i) {
-                    r[bottom, i] = num++;
-                }
-                bottom--;
-
-                for (var i = bottom; i >= top; --i) {
-                    r[i, left] = num++;
-                }
-                left++;
-            }
-
-            return r;
-        }
-
-        public static string PrintMatrix(int[,] m)
-        {
-            var sb = new StringBuilder();
-            int length = m.GetLength(0);
-            for (var i = 0; i < length; ++i) {
-                for (var j = 0; j < length; ++j) {
-                    var num = m[i, j];
-                    sb.AppendFormat("{0,2}", num);
-                    if (j < length - 1) { 
-                        sb.Append(", ");
-                    }
-                }
-                sb.Append('\n');
-            }
-            return sb.ToString();
-        }
+        private int _age;
+        public SpaceAge(int seconds) => _age = seconds;
+        public double OnEarth() => _age / 31557600.0;
+        public double OnMercury() => OnEarth() / 0.2408467;
+        public double OnVenus() => OnEarth() / 0.61519726;
+        public double OnMars() => OnEarth() / 1.8808158;
+        public double OnJupiter() => OnEarth() / 11.862615;
+        public double OnSaturn() => OnEarth() / 29.447498;
+        public double OnUranus() => OnEarth() / 84.016846;
+        public double OnNeptune() => OnEarth() / 164.79132;
     }
-
 
     public class Program
     {
