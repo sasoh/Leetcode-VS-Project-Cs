f1b89ccdd8080553c2652a686777fec2808541c9
Alexander Popov <lxndrppv@gmail.com>
2026-07-06 18:55:44 +0300

    feat: solution to face id 2.0

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -3,22 +3,51 @@ using System.Collections.Generic;
 
 namespace ConsoleApp
 {
-    public static class Pangram
+    public class FacialFeatures
     {
-        public static bool IsPangram(string input)
-        {
-            var freq = new HashSet<char>();
-            foreach (char c in input)
-            {
-                if (!char.IsLetter(c)) continue;
-                var lowercased = char.ToLower(c);
-                freq.Add(lowercased);
-            }
+        public string EyeColor { get; }
+        public decimal PhiltrumWidth { get; }
 
-            return freq.Count == ('z' - 'a' + 1);
+        public FacialFeatures(string eyeColor, decimal philtrumWidth)
+        {
+            EyeColor = eyeColor;
+            PhiltrumWidth = philtrumWidth;
         }
+
+        public override bool Equals(object o) => o != null && o is FacialFeatures f && EyeColor == f.EyeColor && PhiltrumWidth == f.PhiltrumWidth;
+
+        public override int GetHashCode() => (int)((long)EyeColor.GetHashCode() * PhiltrumWidth.GetHashCode() % int.MaxValue);
+    }
+    public class Identity
+    {
+        public string Email { get; }
+        public FacialFeatures FacialFeatures { get; }
+
+        public Identity(string email, FacialFeatures facialFeatures)
+        {
+            Email = email;
+            FacialFeatures = facialFeatures;
+        }
+
+        public override bool Equals(object o) => o != null && o is Identity i && Email == i.Email && FacialFeatures.Equals(i.FacialFeatures);
+
+        public override int GetHashCode() => (int)((long)Email.GetHashCode() * FacialFeatures.GetHashCode() % int.MaxValue);
     }
 
+    public class Authenticator
+    {
+        private readonly HashSet<Identity> _registered = new();
+
+        public static bool AreSameFace(FacialFeatures faceA, FacialFeatures faceB) => faceA.Equals(faceB);
+
+        public bool IsAdmin(Identity identity) => identity.Equals(new Identity("admin@exerc.ism", new FacialFeatures("green", 0.9m)));
+
+        public bool Register(Identity identity) => _registered.Add(identity);
+
+        public bool IsRegistered(Identity identity) => _registered.Contains(identity);
+
+        public static bool AreSameObject(Identity identityA, Identity identityB) => identityA == identityB;
+    }
 
     public class Program
     {
