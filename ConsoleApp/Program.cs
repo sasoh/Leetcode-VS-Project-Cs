ab766262d85234c7315d3d5f20d02f39ac68077a
Alexander Popov <lxndrppv@gmail.com>
2026-07-20 15:29:25 +0300

    feat: solution to leetcode's 1260 shift 2d grid

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,35 +1,36 @@
-﻿namespace ConsoleApp;
+﻿using System.Xml.Linq;
 
-public class HighScores(List<int> scores)
+namespace ConsoleApp;
+
+public class Solution
 {
-    private readonly List<int> _scores = scores;
-
-    public List<int> Scores() => _scores;
-
-    public int Latest() => _scores[^1];
-
-    public int PersonalBest()
+    public IList<IList<int>> ShiftGrid(int[][] grid, int k)
     {
-        var best = int.MinValue;
-        foreach (var score in _scores)
-        {
-            if (score > best) {
-                best = score;
-            }
-        }
-        return best;
-    }
 
-    public List<int> PersonalTopThree()
-    {
-        var s = _scores.ToArray();
-        s.Sort();
-        int min = Math.Min(_scores.Count, 3);
+        // write out grid as a single element array
         var list = new List<int>();
-        for (var i = 0; i < min; ++i) {
-            list.Add(s[^(i + 1)]);
+        foreach (var row in grid) {
+            list.AddRange(row);
         }
-        return list;
+
+        // shift list by k elements
+        for (var i = 0; i < k; ++i) {
+            // take last
+            var last = list[^1];
+            list.RemoveAt(list.Count - 1);
+            list.Insert(0, last);
+            // push in front
+        }
+
+
+        // rebuild the matrix from shifted list
+        var r = new List<IList<int>>();
+        var rows = grid.Length;
+        var cols = grid[0].Length;
+        for (var i = 0; i < rows; ++i) {
+            r.Add(list.GetRange(i * cols, cols));
+        }
+        return r;
     }
 }
 
@@ -37,5 +38,10 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        var s = new Solution();
+        //var p = s.ShiftGrid([[3, 8, 1, 9], [19, 7, 2, 5], [4, 6, 11, 10], [12, 0, 21, 13]], 3);
+        //var p = s.ShiftGrid([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 2);
+        var p = s.ShiftGrid([[3, 8, 1, 9], [19, 7, 2, 5], [4, 6, 11, 10], [12, 0, 21, 13]], 4);
+        Console.WriteLine(p);
     }
 }
