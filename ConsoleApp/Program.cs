03b4c2fd84708376f71c8dd5c1829e0f945974e1
Alexander Popov <lxndrppv@gmail.com>
2026-07-21 11:42:41 +0300

    feat: solution to hamming distance

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,36 +1,15 @@
-﻿using System.Xml.Linq;
+﻿namespace ConsoleApp;
 
-namespace ConsoleApp;
-
-public class Solution
+public static class Hamming
 {
-    public IList<IList<int>> ShiftGrid(int[][] grid, int k)
+    public static int Distance(string firstStrand, string secondStrand)
     {
-
-        // write out grid as a single element array
-        var list = new List<int>();
-        foreach (var row in grid) {
-            list.AddRange(row);
+        if (firstStrand.Length != secondStrand.Length) throw new ArgumentException();
+        var distance = 0;
+        for (var i = 0; i < firstStrand.Length; ++i) {
+            if (firstStrand[i] != secondStrand[i]) distance++;
         }
-
-        // shift list by k elements
-        for (var i = 0; i < k; ++i) {
-            // take last
-            var last = list[^1];
-            list.RemoveAt(list.Count - 1);
-            list.Insert(0, last);
-            // push in front
-        }
-
-
-        // rebuild the matrix from shifted list
-        var r = new List<IList<int>>();
-        var rows = grid.Length;
-        var cols = grid[0].Length;
-        for (var i = 0; i < rows; ++i) {
-            r.Add(list.GetRange(i * cols, cols));
-        }
-        return r;
+        return distance;
     }
 }
 
@@ -38,10 +17,5 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var s = new Solution();
-        //var p = s.ShiftGrid([[3, 8, 1, 9], [19, 7, 2, 5], [4, 6, 11, 10], [12, 0, 21, 13]], 3);
-        //var p = s.ShiftGrid([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 2);
-        var p = s.ShiftGrid([[3, 8, 1, 9], [19, 7, 2, 5], [4, 6, 11, 10], [12, 0, 21, 13]], 4);
-        Console.WriteLine(p);
     }
 }
