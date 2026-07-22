710084547720561c4b159fef552e69db59f50985
Alexander Popov <lxndrppv@gmail.com>
2026-07-22 17:38:35 +0300

    feat: solution to leetcode 1807 evaluate the bracket pairs of a string

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,22 +2,50 @@
 
 namespace ConsoleApp;
 
-// Generate a new string by changing each character in word to its next character in the English alphabet,
-// and append it to the original word.
-// For example, performing the operation on "c" generates "cd" and performing the operation on "zb" generates "zbac".
-// Return the value of the kth character in word, after enough operations have been done for word to have at least k characters.
+using Tag = (int from, int to, string key);
 
 public class Solution
 {
-    public char KthCharacter(int k)
+    public string Evaluate(string s, IList<IList<string>> knowledge)
     {
-        var word = new List<char> {'a'};
-        while (word.Count < k)
+        var tags = ExtractTags(s);
+        var knowledgeDictionary = knowledge.ToDictionary(l => l[0], l => l[1]);
+        var sb = new StringBuilder(s);
+        for (var i = 0; i < tags.Count; i++)
         {
-            word.AddRange(word.Select(t => (char) (t + 1)).ToList());
+            var t = tags[^(i + 1)];
+
+            sb.Remove(t.from, t.to - t.from + 1);
+            if (knowledgeDictionary.TryGetValue(t.key, out var value))
+            {
+                sb.Insert(t.from, value);   
+            }
+            else
+            {
+                sb.Insert(t.from, '?');
+            }
         }
 
-        return word[k - 1];
+        return sb.ToString();
+    }
+
+    private static List<Tag> ExtractTags(string s)
+    {
+        var openBracketIndex = s.IndexOf('(');
+        var values = new List<Tag>();
+        while (openBracketIndex != -1)
+        {
+            var closeBracketIndex = s.IndexOf(')', openBracketIndex);
+            if (closeBracketIndex == -1) continue;
+            values.Add(new Tag(
+                openBracketIndex,
+                closeBracketIndex,
+                s[(openBracketIndex + 1)..(closeBracketIndex)]
+            ));
+            openBracketIndex = s.IndexOf('(', closeBracketIndex);
+        }
+
+        return values;
     }
 }
 
@@ -26,7 +54,6 @@ public class Program
     public static void Main(string[] args)
     {
         var s = new Solution();
-        Console.WriteLine(s.KthCharacter(5));
-        // Console.WriteLine(s.KthCharacter(10));
+        s.Evaluate("(name)is(age)yearsold", [["name","bob"],["age","two"]]);
     }
 }
