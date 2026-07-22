f200e786e617bead09f1036a9aff183d8643164c
Alexander Popov <lxndrppv@gmail.com>
2026-07-22 16:11:34 +0300

    feat: solution to grade school

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,34 +1,46 @@
 ﻿namespace ConsoleApp;
 
-[Flags]
-public enum Allergen
+public class GradeSchool
 {
-    Eggs = 1 << 0,
-    Peanuts = 1 << 1,
-    Shellfish = 1 << 2,
-    Strawberries = 1 << 3,
-    Tomatoes = 1 << 4,
-    Chocolate = 1 << 5,
-    Pollen = 1 << 6,
-    Cats = 1 << 7
-}
+    private readonly Dictionary<int, List<string>> _students = [];
 
-public class Allergies
-{
-    private Allergen _allergies;
-
-    public Allergies(int mask) => _allergies = (Allergen)mask;
-
-    public bool IsAllergicTo(Allergen allergen) => (_allergies & allergen) != 0;
-
-    public Allergen[] List()
+    public bool Add(string student, int grade)
     {
-        var l = new List<Allergen>();
-        foreach (var a in Enum.GetValues<Allergen>()) {
-            if (!IsAllergicTo(a)) continue;
-            l.Add(a);
+        var r = !_students.Any(x => x.Value.Contains(student));
+        if (!_students.ContainsKey(grade))
+        {
+            _students.Add(grade, []);
         }
-        return [.. l];
+
+        if (!_students.Any(x => x.Value.Contains(student)))
+        {
+            _students[grade].Add(student);
+        }
+
+        return r;
+    }
+
+    public IEnumerable<string> Roster()
+    {
+        var orderedGrades = _students.Keys.OrderBy(x => x);
+        foreach (var grade in orderedGrades)
+        {
+            var orderedStudents = _students[grade].OrderBy(x => x);
+            foreach (var student in orderedStudents)
+            {
+                yield return student;
+            }
+        }
+    }
+
+    public IEnumerable<string> Grade(int grade)
+    {
+        if (!_students.TryGetValue(grade, out var students)) yield break;
+        var orderedStudents = _students[grade].OrderBy(x => x);
+        foreach (var student in orderedStudents)
+        {
+            yield return student;
+        }
     }
 }
 
