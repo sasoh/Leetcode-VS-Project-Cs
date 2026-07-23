fea91a68e51f056ba48d52df17c9f8f834fc492f
Alexander Popov <lxndrppv@gmail.com>
2026-07-23 17:24:42 +0300

    feat: solution to matching brackets

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,43 +1,32 @@
 ﻿namespace ConsoleApp;
 
-public class Clock : IEquatable<Clock>
+public static class MatchingBrackets
 {
-    private readonly int _minutes = 0;
-    private readonly int _hours = 0;
+    private static bool IsOpening(char c) => c is '(' or '{' or '[';
+    
+    private static bool IsClosing(char c) => c is ')' or '}' or ']';
+    
+    private static bool CheckMatch(char o, char c) => (o == '(' && c == ')') || (o == '{' && c == '}') || (o == '[' && c == ']');
 
-    public Clock(int hours, int minutes)
+    public static bool IsPaired(string input)
     {
-        var totalMinutes = minutes + hours * 60;
-        while (totalMinutes < 0)
+        var stack = new Stack<char>();
+        foreach (var c in input)
         {
-            totalMinutes = 24 * 60 + totalMinutes;
+            if (IsOpening(c))
+            {
+                stack.Push(c);
+            }
+            else if (IsClosing(c))
+            {
+                if (stack.Count == 0) return false;
+                var last = stack.Pop();
+                if (!CheckMatch(last, c)) return false;
+            }
         }
 
-        _hours = (totalMinutes / 60) % 24;
-        _minutes = totalMinutes % 60;
+        return stack.Count == 0;
     }
-
-    public Clock Add(int minutesToAdd) => new(_hours, _minutes + minutesToAdd);
-
-    public Clock Subtract(int minutesToSubtract) => new(_hours, _minutes - minutesToSubtract);
-
-    public bool Equals(Clock? other)
-    {
-        if (other is null) return false;
-        return _minutes == other._minutes && _hours == other._hours;
-    }
-
-    public override bool Equals(object? obj)
-    {
-        if (obj is null) return false;
-        if (ReferenceEquals(this, obj)) return true;
-        if (obj.GetType() != GetType()) return false;
-        return Equals((Clock) obj);
-    }
-
-    public override int GetHashCode() => HashCode.Combine(_minutes, _hours);
-
-    public override string ToString() => $"{_hours:00}:{_minutes:00}";
 }
 
 public class Program
