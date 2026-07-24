3dd3e063c44bbce49d9f4b1d35951cb73bcbcdab
Alexander Popov <lxndrppv@gmail.com>
2026-07-24 15:24:16 +0300

    feat: solution to collatz conjecture

--- Program.cs
+++ Program.cs
@@ -1,40 +1,30 @@
-﻿using System.Reflection.Metadata.Ecma335;
-
-public enum Classification
+﻿public static class CollatzConjecture
 {
-    Perfect,
-    Abundant,
-    Deficient
-}
-
-public static class PerfectNumbers
-{
-    private static int FactorSum(int number)
+    public static int Steps(int number)
     {
-        var sum = 0;
-        for (int i = 1, limit = number / 2; i <= limit; i++)
+        if (number < 1) throw new ArgumentOutOfRangeException();
+        var counter = 0;
+
+        while (number > 1)
         {
-            if (number % i != 0) continue;
-            sum += i;
+            counter++;
+            if (number % 2 == 0)
+            {
+                number /= 2;
+            }
+            else
+            {
+                number = 3 * number + 1;
+            }
         }
 
-        return sum;
+        return counter;
     }
-
-    public static Classification Classify(int number) => number < 1
-            ? throw new ArgumentOutOfRangeException()
-            : (number - FactorSum(number)) switch
-            {
-                > 0 => Classification.Deficient,
-                0 => Classification.Perfect,
-                _ => Classification.Abundant
-            };
 }
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine(PerfectNumbers.Classify(12));
     }
 }
