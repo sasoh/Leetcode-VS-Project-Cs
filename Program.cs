c13b340d95089d57cabffeb79bb33fdfc80a4276
Alexander Popov <lxndrppv@gmail.com>
2026-07-24 15:21:31 +0300

    feat: solution to perfect numbers

--- Program.cs
+++ Program.cs
@@ -1,6 +1,40 @@
-﻿public class Program
+﻿using System.Reflection.Metadata.Ecma335;
+
+public enum Classification
+{
+    Perfect,
+    Abundant,
+    Deficient
+}
+
+public static class PerfectNumbers
+{
+    private static int FactorSum(int number)
+    {
+        var sum = 0;
+        for (int i = 1, limit = number / 2; i <= limit; i++)
+        {
+            if (number % i != 0) continue;
+            sum += i;
+        }
+
+        return sum;
+    }
+
+    public static Classification Classify(int number) => number < 1
+            ? throw new ArgumentOutOfRangeException()
+            : (number - FactorSum(number)) switch
+            {
+                > 0 => Classification.Deficient,
+                0 => Classification.Perfect,
+                _ => Classification.Abundant
+            };
+}
+
+public class Program
 {
     public static void Main(string[] args)
     {
+        Console.WriteLine(PerfectNumbers.Classify(12));
     }
 }
