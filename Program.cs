8c9025c5fdbc6518008d9588f373577c64b1e0ff
Alexander Popov <lxndrppv@gmail.com>
2026-07-26 10:52:38 +0300

    feat: solution to phone number

--- Program.cs
+++ Program.cs
@@ -1,24 +1,21 @@
-﻿public static class CollatzConjecture
-{
-    public static int Steps(int number)
-    {
-        if (number < 1) throw new ArgumentOutOfRangeException();
-        var counter = 0;
+﻿using System.Text;
 
-        while (number > 1)
+public class PhoneNumber
+{
+    public static string Clean(string phoneNumber)
+    {
+        var sb = new StringBuilder();
+        for (var i = 0; i < phoneNumber.Length; i++)
         {
-            counter++;
-            if (number % 2 == 0)
-            {
-                number /= 2;
-            }
-            else
-            {
-                number = 3 * number + 1;
-            }
+            char c = phoneNumber[i];
+            if (!char.IsDigit(c)) continue;
+            if (sb.Length == 0 && c == '1') continue;
+            sb.Append(c);
         }
 
-        return counter;
+        if (sb.Length != 10 || int.Parse(sb[0].ToString()) < 2 || int.Parse(sb[3].ToString()) < 2) throw new ArgumentException();
+
+        return sb.ToString();
     }
 }
 
@@ -26,5 +23,10 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        Console.WriteLine(PhoneNumber.Clean("+1 (613)-995-0253"));
+        Console.WriteLine(PhoneNumber.Clean("613-995-0253"));
+        Console.WriteLine(PhoneNumber.Clean("1 613 995 0253"));
+        Console.WriteLine(PhoneNumber.Clean("613.995.0253"));
+        Console.WriteLine(PhoneNumber.Clean("(023) 456-7890"));
     }
 }
