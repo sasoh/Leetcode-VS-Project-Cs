d07dae34e7f4fdf40f09e39f7f218936b1c21b6b
Alexander Popov <lxndrppv@gmail.com>
2026-07-27 14:21:01 +0300

    feat: solution to series

--- Program.cs
+++ Program.cs
@@ -1,63 +1,16 @@
-﻿public static class ResistorColorTrio
+﻿public static class Series
 {
-    private static long ResistanceForKey(string key) => key switch
+    public static string[] Slices(string numbers, int sliceLength)
     {
-        "black" => 0L,
-        "brown" => 1L,
-        "red" => 2L,
-        "orange" => 3L,
-        "yellow" => 4L,
-        "green" => 5L,
-        "blue" => 6L,
-        "violet" => 7L,
-        "grey" => 8L,
-        "white" => 9L,
-        _ => throw new NotImplementedException(),
-    };
+        if (sliceLength < 1 || numbers.Length < sliceLength) throw new ArgumentException();
+        ArgumentException.ThrowIfNullOrEmpty(numbers);
 
-    public static string Label(string[] colors)
-    {
-        var sum = 0L;
-        for (var i = 0; i < 2; i++)
-        {
-            var resistance = ResistanceForKey(colors[i]);
-            sum += resistance * (long)Math.Pow(10, 1 - i);
-        }
-        var zeroes = ResistanceForKey(colors[2]);
-        sum *= (long)Math.Pow(10, zeroes);
-        var totalZeroes = CountZeroes(sum);
-        var toTrim = (totalZeroes / 3) * 3;
-        long trimmedSum = TrimmedSum(sum, toTrim);
-        var suffix = toTrim switch
-        {
-            3 => "kilo",
-            6 => "mega",
-            9 => "giga",
-            _ => ""
-        };
-        return $"{trimmedSum} {suffix}ohms";
-    }
-
-    private static long TrimmedSum(long sum, long toTrim)
-    {
-        for (var i = 0L; i < toTrim; ++i)
-        {
-            sum /= 10;
+        var slices = new List<string>();
+        for (int i = 0, limit = numbers.Length - sliceLength; i <= limit; i++) {
+            slices.Add(numbers[i..(i + sliceLength)]);
         }
 
-        return sum;
-    }
-
-    private static long CountZeroes(long sum)
-    {
-        var totalZeroes = 0L;
-        while (sum % 10 == 0 && sum > 0)
-        {
-            totalZeroes++;
-            sum /= 10;
-        }
-
-        return totalZeroes;
+        return [.. slices];
     }
 }
 
@@ -65,12 +18,6 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        //Console.WriteLine(ResistorColorTrio.Label(["orange", "orange", "black"]));
-        //Console.WriteLine(ResistorColorTrio.Label(["red", "black", "red"]));
-        //Console.WriteLine(ResistorColorTrio.Label(["black", "grey", "black"]));
-        //Console.WriteLine(ResistorColorTrio.Label(["blue", "grey", "brown"]));
-        //Console.WriteLine(ResistorColorTrio.Label(["yellow", "violet", "yellow"]));
-        Console.WriteLine(ResistorColorTrio.Label(["white", "white", "white"]));
-        Console.WriteLine(ResistorColorTrio.Label(["blue", "green", "yellow", "orange"]));
+        Console.WriteLine(string.Join(", ", Series.Slices("123456", 3)));
     }
 }
